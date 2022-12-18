3a18a3cd11ead8afad7ea2b10151f26d6933ddae
noorchalim <noorchalim3@gmail.com>
2022-12-18 21:21:05 +0700

    permision geolocation

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+function success(position){
+    console.log(position);
+}
+
+function error(){
+    alert('posisi tidak dapat diakses');
+}
+
+function userLocation(){
+    if(!navigator.geolocation){
+        alert('browser tidak mendukung geolocation');
+    }else{
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
 function index(){
     let app = document.getElementById('app');
     let h3 = document.createElement('h3');
     h3.innerHTML = 'Prayer Time';
 
     app.appendChild(h3);
+
+    userLocation();
 }
 index();
