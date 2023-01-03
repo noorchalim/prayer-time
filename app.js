e77043e5a2267caf664b64eebdd81978150796a8
noorchalim <noorchalim3@gmail.com>
2023-01-03 17:26:21 +0700

    membuat table untuk jadwal sholat

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ function prayerTimes(latitude, longitude){
     .then(response => response.json())
     .then(function(response){
         let date = new Date();
-        let today = date.getData() - 1;
-        console.log(today);
-        console.log(response.data[today]);
+        let today = date.getDate() - 1;
+        let data = response.data[0].timings;
+
+        let app = document.getElementById('app');
+        let table = document.createElement('table');
+        let tableTbody = document.createElement('Tbody');
+
+        for(i in data){
+            let row = tableTbody.insertRow();
+            let name = row.insertCell(0);
+            let time = row.insertCell(1);
+            name.innerHTML = i;
+            time.innerHTML = data[i];
+            tableTbody.appendChild(row);
+            
+        }
+        table.appendChild(tableTbody);
+        app.appendChild(table);
+
+
+        // console.log(data);
+        // console.log(today);
+        // console.log(response.data[today]);
     });
 }
 
